88365478e4dc87c0a74c0af1cb4fc63c03d24392
alexatgetcalvinapp <alex@getcalvin.app>
2025-02-05 20:49:46 +0200

    Initial Commit

---
+++ FirebaseSignInWithAppleDemo/FirebaseSignInWithAppleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSignInWithAppleDemoApp.swift
+//  FirebaseSignInWithAppleDemo
+//
+//  Created by Alex Nagy on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSignInWithAppleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
