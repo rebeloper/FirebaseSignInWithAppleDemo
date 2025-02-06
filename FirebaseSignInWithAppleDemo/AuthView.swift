403ce640550b48d2234ffa6c85bb55122b33cfb7
alexatgetcalvinapp <alex@getcalvin.app>
2025-02-06 13:49:56 +0200

    firebase sign in with apple

---
+++ FirebaseSignInWithAppleDemo/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  FirebaseSignInWithAppleDemo
+//
+//  Created by Alex Nagy on 06.02.2025.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
